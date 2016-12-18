13444f941db5e28a4a04f399f55ba883e67f8da3
schraibi <selma.chraibi@efpl.ch>
2016-12-18 18:52:28 +0100

    change folder

--- Notebooks/fonctions_auxiliaires.py
+++ Notebooks/fonctions_auxiliaires.py
@@ -1,23 +1,27 @@
 import re
-import datetime  
 import time
 from sklearn.metrics import confusion_matrix
+from hpsklearn import demo_support, components
+import matplotlib.pyplot as plt
 
-def retriever(estimator):
+list_classifier = [ 'svc', 'knn', 'random_forest', 'extra_trees', 'ada_boost', 'gradient_boosting', 'sgd']
+list_preprocessor =['pca', 'standard_scaler', 'min_max_scaler', 'normalizer', 'None']
+
+
+def time_retriever(estimator):
     classifier_values = estimator.trials.trials
     timestamp = {}
-    classifier_param = {}
     
     for i in range(len(classifier_values)):
-        time['i'] = classifier_values[i]['book_time']
-        timestamp['i'] = time.mktime(time['i'].timetuple())
-        iteration[''] = classifier_values[i]['misc']['vals']
-
+        book_time = classifier_values[i]['book_time']
+        timestamp['i'] = time.mktime(book_time.timetuple())
+  
+    return timestamp
 
 ####    
 
 def predict_intermediate(classifiers, X_test):
-    estim = estimator.hyperopt_estimator(classifier = classfiers
+    estim = estimator.hyperopt_estimator(classifier = classfiers)
 
     for i in range(classifiers.shape[0]):
         clf = classifiers[i][0]
@@ -29,16 +33,25 @@ def predict_intermediate(classifiers, X_test):
 
 ####
 
-def metrics(estimator, X_train, y_train, X_test, y_test, y)
+def metrics(estimator, X_train, y_train, X_test, y_test, y):
     train_score = estimator.score(X_train, y_train)
     test_score = estimator.score(X_test, y_test)
     conf_matrix = confusion_matrix(y_true=y_test, y_pred=y)
-    precision = conf_matrix[0,0]/(conf_matrix[0,0]+conf_matrix[0,1]
-    recall = conf_matrix[0,0]/(conf_matrix[0,0]+conf_matrix[1,0]
-    F = 2*(precison*recall)/(precision + recall)
+    precision = conf_matrix[0,0]/(conf_matrix[0,0]+conf_matrix[0,1])
+    recall = conf_matrix[0,0]/(conf_matrix[0,0]+conf_matrix[1,0])
+    F = 2*(precision*recall)/(precision + recall)
     
-    return train_score, test_score, precison, recall, F
+    return train_score, test_score, precision, recall, F
 
 
 ####
 
+def plot_perf(estimator):
+    
+    fig = plt.figure()
+
+    ax= fig.add_subplot(111)
+    demo_support.scatter_error_vs_time(estimator, ax)
+    ax2 = fig.add_subplot(222)
+    return demo_support.plot_minvalid_vs_time(estimator, ax2)
+  
